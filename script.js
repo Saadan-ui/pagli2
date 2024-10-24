8a6a4f738da3e38aa27f20b4cbeb342f20c00870
Saadan-ui <saadan305@gmail.com>
2024-10-24 17:49:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ window.onload = function(){
     var baloons = document.querySelector(".heartBaloons");
     baloons.style.top = "-170vh";
 };
+
+// on click grayscale change
+
+function gray1(){
+    var us1 = document.querySelector("#us1").style.filter = "grayscale(0)";    
+}
+function gray2(){
+    var us2 = document.querySelector("#us2").style.filter = "grayscale(0)";    
+}
+function gray3(){
+    var us3 = document.querySelector("#us3").style.filter = "grayscale(0)";    
+}
+
+// let c = confirm("Will you");
+// if(c){
+//     count = count + 1;
+// }
+// else{
+//     alert("achha")
+// }
